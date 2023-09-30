8d12aa0445ec1fc064dfff07f482d03071373c89
Barath <iambharathpadhu@gmail.com>
2023-09-30 16:48:12 +0530

    - initial commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
+import React from "react";
+import TypeWriterEffect from "react-typewriter-effect";
+import { motion } from "framer-motion";
 
-function Intro() {
-   return (
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Forrest</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
-         <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
-            <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://youtube.com/fknight"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               ForrestKnight
-            </a>{' '}
-            to more than 400k subscribers.
-         </p>
-      </div>
-   )
+function Intro({ theme }) {
+  console.log("Theme", theme);
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <TypeWriterEffect
+        textStyle={{
+          fontSize: "32px",
+          fontWeight: "800",
+          color: "#333333",
+          lineHeight: "1.9",
+          margin: "10px 0",
+          color: theme === "dark" ? "white" : "black",
+          textAlign: "center",
+        }}
+        startDelay={500}
+        cursorColor="black"
+        text="Barath's Portfolio"
+        typeSpeed={100}
+      />
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.75 }}
+      >
+        <img
+          src="https://avatars.githubusercontent.com/u/53510578?s=400&u=cbc82579d3c19d480e3b59fc1562d5c1bd4f6c48&v=4"
+          className="rounded-full"
+        />
+      </motion.div>
+      <p className="text-base md:text-xl mb-3 font-medium pt-2">
+        Software Development Engineer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Hello there! My name is Barath, and I hail from the vibrant city of
+        Coimbatore. Beyond being a passionate resident of this beautiful place,
+        I'm an ardent Messi fan, always cheering for him on the field. In my
+        free time, I'm not just a spectator; I'm also a creator. I express my
+        thoughts and ideas through writing, particularly in the form of blogs.
+        My blog topics vary, but I often find myself delving into the world of
+        technology, exploring its latest innovations, and sharing my insights.
+        When I'm not immersed in the digital realm, you can find me hanging out
+        with my friends. I cherish those moments of camaraderie, as they are the
+        essence of life's enjoyment. One motto that I live by is "under promise
+        and over deliver." This principle guides my actions and helps me strive
+        for excellence in all that I do. Whether it's in my writing, my
+        interactions with friends, or my pursuit of technological knowledge, I
+        believe in setting realistic expectations and then surpassing them. So,
+        that's a little glimpse into who I am - a Messi-loving, technology
+        enthusiast, blogging, and friend-enjoying individual who aims to exceed
+        expectations in every aspect of life.
+      </p>
+    </div>
+  );
 }
 
 export default Intro;
